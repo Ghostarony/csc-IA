346ae3def569fc007e004352066a46fd86a56c7f
Ghost <aino.laiho@gmail.com>
2024-09-09 11:41:17 +0300

    initial commit

---
+++ CONTROLLER.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.SwingUtilities;
+
+public class CONTROLLER {
+    public static List<Work> works = new ArrayList<>(); //creates a queue to store works in
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(()-> new APP());
+    }
+}
