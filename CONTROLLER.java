fd0eeffbdc7135b0d3f6f2275b18326906bcc23c
Ghost <aino.laiho@gmail.com>
2024-09-16 10:54:53 +0300

    new list name

--- CONTROLLER.java
+++ CONTROLLER.java
@@ -4,7 +4,7 @@ import java.util.List;
 import javax.swing.SwingUtilities;
 
 public class CONTROLLER {
-    public static List<Work> works = new ArrayList<>(); //creates a queue to store works in
+    public static List<Work> workList = new ArrayList<>(); //creates a queue to store works in
     public static void main(String[] args) {
         SwingUtilities.invokeLater(()-> new APP());
     }
